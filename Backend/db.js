3a539f6c59399acc7ce2c4cfc57665f40321dd3d
Pratyush <102897186+Kamisama9@users.noreply.github.com>
2024-07-28 12:20:42 +0530

    Update db.js

--- Backend/db.js
+++ Backend/db.js
@@ -5,7 +5,7 @@ dotenv.config()
 
 const connectDB = async () => {
     try {
-        await mongoose.connect(MONGO_URI);
+        await mongoose.connect(process.env.MONGO_URI);
         console.log('MongoDB database connected successfully.');
     } catch (error) {
         console.error('MongoDB database connection failed. Error:', error.message);
